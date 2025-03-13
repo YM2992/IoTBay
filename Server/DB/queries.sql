10c183b41b06255a61002a7695c0224e6642a654
Jeffrey <562475805lanu@gmail.com>
2025-03-14 10:30:22 +1100

    Added short cut for npm start

--- Server/DB/queries.sql
+++ Server/DB/queries.sql
@@ -1,9 +1,7 @@
 CREATE TABLE user (
-    -- student_id SERIAL PRIMARY KEY,
-    -- name VARCHAR(100) NOT NULL,
-    -- address VARCHAR(255),
-    -- age INT,
-    -- gender VARCHAR(10),
-    -- department_id INT,
-    -- FOREIGN KEY (department_id) REFERENCES Departments(department_id)
+    userid AUTOINCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone int NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
 );

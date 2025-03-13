82876c77b5c5e56a20846c6dbfe7b628cdefed56
Jeffrey <562475805lanu@gmail.com>
2025-03-14 10:39:38 +1100

    First DB created

--- Server/DB/queries.sql
+++ Server/DB/queries.sql
@@ -1,7 +1,12 @@
-CREATE TABLE user (
-    userid AUTOINCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    phone int NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL
-);
+-- CREATE TABLE user (
+--     userid INTEGER PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     phone int NOT NULL,
+--     email VARCHAR(100) NOT NULL UNIQUE,
+--     password VARCHAR(100) NOT NULL
+-- )
+
+INSERT INTO user(name, phone, email, password)
+VALUES('Jeff', 0424123456, 'test@gmail.com', '1234');
+
+SELECT * FROM user;

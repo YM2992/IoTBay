4275250475c7430be18d9211e29e308c76183662
Yasir Mustafa <yasirm427@gmail.com>
2025-03-15 12:54:29 +1100

    Added landing page routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import "./App.css";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/landing" element={<Landing />} />
         </Route>
       </Routes>
     </BrowserRouter>

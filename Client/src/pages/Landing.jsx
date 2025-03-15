66434a7c295784824531988475a05f19d34a08b0
Yasir Mustafa <yasirm427@gmail.com>
2025-03-15 12:54:09 +1100

    Initial draft of landing page

--- Client/src/pages/Landing.jsx
+++ Client/src/pages/Landing.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import './Landing.css';
 
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div className="landing-page">
+      <main>
+        <section className="hero">
+          <h1 className="hero-h1">Welcome to IoTBay</h1>
+          <h2 className="hero-h2">Your one stop shop for all IoT needs</h2>
+          <p className="hero-p">Discover a wide range of sensors, actuators, and components for your IoT projects</p>
+          <a href="/main" className="hero-button">Explore our range of devices</a>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default Landing;

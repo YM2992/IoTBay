e1b36d8a4266ac5387360621c0064e2c1206a689
Yasir Mustafa <yasirm427@gmail.com>
2025-03-21 10:23:29 +1100

    Update Landing.jsx

--- Client/src/pages/Landing.jsx
+++ Client/src/pages/Landing.jsx
@@ -8,7 +8,7 @@ function Landing() {
         <section className="hero">
           <h1 className="hero-h1">Welcome to IoTBay</h1>
           <h2 className="hero-h2">Your one stop shop for all IoT needs</h2>
-          <p className="hero-p">Discover a wide range of sensors, actuators, and components for your IoT projects</p>
+          <p className="hero-p">Discover our wide range of IoT components for your projects</p>
           <a href="/main" className="hero-button">Explore our range of devices</a>
         </section>
       </main>

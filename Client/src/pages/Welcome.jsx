7ee7278ef7238e3cfdaffcaeafe40309a08e6838
John <johnlin8968@gmail.com>
2025-03-28 12:38:20 +1100

    update welcome.jsx

--- Client/src/pages/Welcome.jsx
+++ Client/src/pages/Welcome.jsx
@@ -3,6 +3,9 @@ import React from "react";
 // After login redirect to welcome page and redirect to main again?
 
 function Welcome() {
+  const token = localStorage.getItem("jwt");
+
+  
   return (
     <>
       <div>Welcome</div>

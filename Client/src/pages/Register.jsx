fd19e342f23cc11a4efea4cb61f0b93c298176fd
Melisa <melisa@MELI>
2025-03-28 11:25:57 +1100

    All fields

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -2,30 +2,40 @@ import React from "react";
 import "./Register.css";
 
 function Register(){
+
     return(
     <div className="register-page">
         <main>
             <h1>Registration</h1>
             <h2>Join us for an easier shopping experience</h2>
 
-            <div class="FieldMargin">
-                <label for="fullName">Full Name</label>
-                <input type="text" id="fullName" name="Full Name" required/>                
-            </div>
+            <div class="centered">
+                           
+                {/*Does this save?*/}
+                <div class="FieldMargin">
+                    <label for="fullName">Full Name</label>
+                    <input type="text" id="fullName" name="Full Name" required/>                
+                </div>
 
-            <div class="FieldMargin">
-                <label for="getPass">Password</label>
-                <input type="password" id="getPas" name="password" minLength="8" required/>                
-            </div>
-            
-            <div class="FieldMargin">    
-                <label for="getPass">Confirm Password</label>
-                <input type="password" id="confirmPas" name="confirm_password" />
-            </div>
+                <div class="FieldMargin">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="Full Name" required/>                
+                </div>
 
-            
-            <button type="submit" class="btn"> Submit </button>
-            
+                <div class="FieldMargin">
+                    <label for="getPass">Password</label>
+                    <input type="password" id="getPas" name="password" minLength="8" required/>                
+                </div>
+
+                <div class="FieldMargin">    
+                    <label for="getPass">Confirm Password</label>
+                    <input type="password" id="confirmPas" name="confirm_password" autoComplete="off" />
+                </div>
+
+                {/* This needs to returned red text error if any validations fails*/}                            
+                <button type="submit" class="btn"> Submit </button>              
+
+            </div>
         </main>        
     </div>
     )

6dea24c9c85b0bd4e5c3e921e6ae9aa5ca4b47ac
Yasir Mustafa <yasirm427@gmail.com>
2025-03-28 11:33:38 +1100

    Added conditional rendering of pages from user authentication

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 
 import Layout from "./components/Layout";
-import Home from "./pages/Home";
 import MainPage from "./pages/MainPage";
 import Login from "./pages/Login";
 import Landing from "./pages/Landing";
@@ -14,10 +13,43 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Landing />} />
           <Route path="/landing" element={<Landing />} />
+
+          {/* Conditional rendering if user is authenticated */}
+          <Route path="/login"
+            element={
+              localStorage.getItem("token") ? (
+                // logged in, show welcome page
+                (() => {
+                  return <Navigate to="/welcome" replace />
+                })()
+              ) : (
+                // not logged in, allow login
+                <Login />
+              )
+            }
+          />
+          <Route path="/main"
+            element={
+              localStorage.getItem("token") ? <MainPage /> : <Navigate to="/login" replace />
+            }
+          />
+          <Route path="/logout"
+            element={
+              localStorage.getItem("token") ? (
+                // logged in, clear user session and redirect to landing
+                (() => {
+                  localStorage.clear();
+                  return <Navigate to="/landing" replace />;
+                  // return <Logout />;
+                })()
+              ) : (
+                // not logged in, redirect to landing
+                <Navigate to="/" replace />
+              )
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>

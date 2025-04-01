deea1fc43b2013d24c0eaf4b96f5cac90c68a9eb
John <johnlin8968@gmail.com>
2025-04-01 12:59:37 +1100

    updated styling

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -6,31 +6,29 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-const handleSubmit = async () =>{
-  if(email.trim === "" || password.trim === "") return "";
-  const data = {
-    email,password
-  }
+  const handleSubmit = async () => {
+    if (email.trim === "" || password.trim === "") return "";
+    const data = {
+      email,
+      password,
+    };
 
-  const res = await fetch("http://localhost:8000/api/user/login/",{
+    const res = await fetch("http://localhost:8000/api/user/login/", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // if res.status. == fail code != 200
 
-    method:"POST",
-    body:JSON.stringify(data),
-    headers:{
-      "Content-Type" :"application/json",
-    } 
-  })
-  // if res.status. == fail code != 200
-
-  // got the 
-  const resData = await res.json();
-  console.log(resData.token);
-  localStorage.setItem("jwt", resData.token);
-
-}
+    // got the
+    const resData = await res.json();
+    console.log(resData.token);
+    localStorage.setItem("jwt", resData.token);
+  };
 
   return (
-
     <div className="login-container">
       <div className="login-card">
         <div className="center-content">
@@ -40,19 +38,19 @@ const handleSubmit = async () =>{
           <h2 className="welcome-text">Welcome!</h2>
           <p className="info-text">Please sign in to your account below</p>
         </div>
-        
-    
-        <Input field="email" func={setEmail} className="input-field"/>
-        <Input field="password" func={setPassword} type="password" />
-    
-          <div className="forgot-password">
-            <a href="#">Forgot Password?</a>
-          </div>
 
-          <button onClick={handleSubmit} type="submit" className="sign-in-btn">
-            Sign in
-          </button>
+        <div className="input-container">
+          <Input field="email" func={setEmail} className="input-field" />
+          <Input field="password" func={setPassword} type="password" />
+        </div>
 
+        <div className="forgot-password">
+          <a href="#">Forgot Password?</a>
+        </div>
+
+        <button onClick={handleSubmit} type="submit" className="sign-in-btn">
+          Sign in
+        </button>
 
         <p className="contact-us">
           Having problems? <a href="#">Contact us</a>

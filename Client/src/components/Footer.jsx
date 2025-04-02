a2ee6dee86e4ff6a88132e016fce5aea48a0112c
Jeffrey <562475805lanu@gmail.com>
2025-04-02 18:03:17 +1100

    Update Footer

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <div className="footer">&copy; IoTBay 2025. All rights reserved.</div>;
+  return (
+    <div className="footer">
+      <p>&copy; IoTBay 2025. All rights reserved.</p>
+      <p>Created by Team IoTBay [ Gordon, Jeff, Jin, John, Melisa, Yasir ]</p>
+    </div>
+  );
 }
 
 export default Footer;

cb5285ea17ecf956ab58b2fb59f37c1aca1c5233
Gilly <gordon.l.gill@student.uts.edu.au>
2025-05-08 17:58:47 +1000

    Update cartRoute.js

--- Server/Route/cartRoute.js
+++ Server/Route/cartRoute.js
@@ -9,11 +9,8 @@ import { protect } from "../Controller/authController.js";
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.use(protect); // ✅ Make sure this is applied BEFORE all routes
 
-=======
->>>>>>> 657c19a85019d129818acb9b7f53d8540388198e
 router.post("/add", addToCart);
 router.get("/", getCartItems);
 router.post("/update-quantity", updateCartQuantity);
